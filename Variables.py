2cd3a1cae578bab7369479835a52567ced536245
Saloni Sharma <your-email@example.com>
2025-09-12 15:00:02 +0530

    Initial commit with practice files

---
+++ Variables.py
@@ -0,0 +1,38 @@
+# variable = a container for a value 
+# a variable behaves as if it is the value it contains
+
+# string = a series of character 
+first_name = "saloni"
+food = "pizza"
+email = "ss3910241@gmail.com"
+
+print(f"hello {first_name}.")
+print(f"you like {food}.")
+print(f"your email is {email}.")
+
+#integers
+age = 20
+quantity = 3 
+num_of_students = 30
+
+print(f"your age is {age}.")
+print(f"you are buying a total of {quantity} items.")
+print(f"the total number of students in your class are {num_of_students}.")
+
+#float
+price = 12.9
+distance = 5.5
+gpa = 3.2
+
+print(f"the price of that item is ${price}.")
+print(f"you walked a total of {distance} km.")
+print(f"your gpa is {gpa}.")
+
+#boolean
+is_student = True
+
+if is_student:
+    print("you are a student.")
+
+else: 
+    print("you are not a student")
